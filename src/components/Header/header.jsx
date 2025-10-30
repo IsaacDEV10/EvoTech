7f6faa3124c5f4987203e80e0f617edf7d78d64c
Isaac Luiz <isaacluiz27@hotmail.com>
2025-10-30 10:09:14 -0300

    alteraçoes estilos

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+
 import './header.css'
+import React, { useState } from 'react';
 import logo from '../../assets/EvoTech.svg'
 import Contact from '../Contact/contact'
+import MobileNav from '../MobileNav/MobileNav';
+
+
+
 
 
 const Header = () => {
-  return (
-    <nav className='nav-wrapper'>
-        <div className='nav-content'>
-            <img className='logo' src={logo} alt="" />
-            <ul>
-                <li>
-                    <a className='menu-item' href="#home">Home</a>
-                </li>
 
-                <li>
-                    <a className='menu-item' href="#sobre">Sobre</a>
-                </li>
+    const [openMenu, setOpenMenu] = useState(false);
 
-                <li>
-                    <a className='menu-item' href="#projetos">Projetos</a>
-                </li>
-                <Contact/>
-            </ul>
-        </div>
-    </nav>
-  )
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
+    return (
+        <>
+            <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+            <nav className='nav-wrapper'>
+                <div className='nav-content'>
+                    <img className='logo' src={logo} alt="" />
+                    <ul>
+                        <li>
+                            <a className='menu-item' href="#home">Home</a>
+                        </li>
+
+                        <li>
+                            <a className='menu-item' href="#sobre">Sobre</a>
+                        </li>
+
+                        <li>
+                            <a className='menu-item' href="#projetos">Projetos</a>
+                        </li>
+                        <Contact />
+                    </ul>
+                    <button className="menu-btn" onClick={toggleMenu}></button>
+                    <span>=</span>
+                </div>
+            </nav>
+        </>
+    )
 }
+
 
 export default Header
